330936975b22661ab2a853c6f34bd392e99fec29
Anata Arisa <vinhnqse170501@fpt.edu.vn>
2022-05-26 20:22:45 +0700

    V3

---
+++ demo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void f4(){
+    int firstNum = 0;
+    int secondNum = 0;
+    printf("first num input:");
+    scanf("%d",&firstNum);
+    printf("\nSecond num input:");
+    scanf("%d",&secondNum);
+    printf("Tich cua num 1 va 2 la: %d", firstNum * secondNum);
+}
+int main()
+{
+    printf("Hello F-Code:\n");
+    f4();
+    return 0;
+}
