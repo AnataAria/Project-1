d7c71c5721f22bfe0fdc97286127c90a635bb6a5
Anata Arisa <vinhnqse170501@fpt.edu.vn>
2022-05-26 20:43:25 +0700

    V4

--- demo.c
+++ demo.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int f1(){
  printf("Project1: kim - han - vinh");
 }
@@ -8,11 +7,21 @@ int minus(){
 	scanf("%d %d", &a, &b);
 	printf("Minus: %d", a-b);	
 };
+void f4(){
+    int firstNum = 0;
+    int secondNum = 0;
+    printf("first num input:");
+    scanf("%d",&firstNum);
+    printf("\nSecond num input:");
+    scanf("%d",&secondNum);
+    printf("Tich cua num 1 va 2 la: %d", firstNum * secondNum);
+}
 int main()
 {
 	int a, b;
     printf("Hello F-Code:\n");
     f1();
     minus();
+    f4();
     return 0;
 }
